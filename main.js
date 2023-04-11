37004140f1cf8672a3e751a811b6d018b5d1d714
srijan4500 <118520625+srijan4500@users.noreply.github.com>
2023-04-12 03:07:08 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,99 @@
+const nav = document.querySelector("#nav");
+
+const onScroll = () => {
+    const scrollPosition = window.scrollOffsetY;
+
+    nav.classList
+       .toggle(
+        "scrolled-down",
+        scrollPosition > 56
+       );
+};
+function trunate(str ,n){
+    return str?.length > n ? str.substr(0,n-1) + "..." : str;
+  }
+document.addEventListener(
+    "scroll",
+    onScroll,{passive : true}
+);
+
+function search(query) {
+	const url = `https://api.tvmaze.com/search/shows?q=${query}`;
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => resultPrint(data));
+}
+function loadHome() {
+	for (let i = 1; i < 101; i++) {
+		let targetUrl = `https://api.tvmaze.com/shows/${i}`;
+		fetch(targetUrl)
+			.then((response) => response.json())
+			.then((data) => renderElement(data));
+	}
+}
+window.onload = () => {
+	loadHome();
+	const searchBar = document.getElementById('searchBar');
+	searchBar.onkeyup = (event) => {
+		search(searchBar.value);
+	};
+};
+function renderElement(data) {
+	const listH = document.getElementById('listH');
+	let element = document.createElement('div');
+	element.innerHTML = `
+    <div class="card" onclick="window.location.href = '${data.url}';">
+        <img src="${data.image.medium}"  alt="Movie Image">
+        <div class="card-overlay">
+          <h3 class="card-title">${data.name}</h3>
+          <p class="card-desc">${trunate(data.summary,50)}</p>
+          <p class="card-duration">Duration: ${data.averageRuntime}</p>
+          <p class="card-rating">Rating: ${data.rating.average}</p>
+        </div>
+    </div>`;
+	element.style.display = 'flex';
+	element.style.float = 'left';
+	element.style.marginRight = '35px';
+	element.style.cursor = 'pointer';
+	listH.appendChild(element);
+}
+
+function resultPrint(data) {
+	const list = document.getElementById('list');
+	let nameArr = data.map((element) => element.show.name);
+	let imgArr = data.map((element) => element.show.image);
+	let summaryArr = data.map((element) => element.show.summary);
+	let ratingArr = data.map((element) => element.show.rating.average);
+	let runtimeArr = data.map((element) => element.show.averageRuntime);
+	let urlArr = data.map((element) => element.show.url);
+	list.innerHTML = '';
+	imgArr.forEach((image, i) => {
+		let element = document.createElement('div');
+		element.style.display = 'flex';
+		element.style.float = 'left';
+		element.style.marginLeft = '10px';
+		element.style.marginRight = '1rem';
+		let img = document.createElement('img');
+		img.src = image.medium;
+		img.style.borderRadius = '10px';
+		let title = document.createElement('p');
+		title.innerText = nameArr[i];
+		title.style.fontSize = '20px';
+		title.style.alignSelf = 'center';
+
+		const details = document.createElement('div');
+		details.className = 'details';
+		details.innerHTML = `
+        <a id='headingMov' href=${urlArr[i]}>${nameArr[i]}</a>
+        <p>${summaryArr[i]}</p>
+        <p><b>Rating:</b> ${ratingArr[i]}</p>
+        <p><b>Duration:</b> ${runtimeArr[i]} min</p>
+    `;
+		element.style.marginBottom = '30px';
+		details.style.margin = '20px';
+		details.style.display = 'inline';
+		element.appendChild(img);
+		element.appendChild(details);
+		list.appendChild(element);
+	});
+}
